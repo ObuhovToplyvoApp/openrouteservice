54cc3cbc4df7564ddf0975f59024410a0bd9cde8
Серега Обухов <obuhov.s@toplyvo.app>
2020-05-18 18:34:38 +0300

    Add bash script and chmod changes

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-wget https://download.geofabrik.de/europe/monaco-latest.osm.pbf -O docker/data/main.pbf;
+wget https://download.geofabrik.de/europe-latest.osm.pbf -O docker/data/main.pbf;
 
 cd docker; ./restart.sh ;
 
